9924d9c97df90f8b26d24142cbd99fec84460114
seesaw game <tttjjjccc@gmail.com>
2021-02-03 13:30:20 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.send('Hello, Oracle.');
+  res.status(200);
   res.render('index', { title: 'Oracle API Practice' })
-  next();
+  // next();
 });
 
 module.exports = router;
